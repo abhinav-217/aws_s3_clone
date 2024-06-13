0e7d876c71e6ac828e1d5f531715c65eff4e8217
abhinav-217 <abhinavsaxena217@gmail.com>
2024-06-14 04:31:29 +0530

    Recieving files

--- router/file_object.js
+++ router/file_object.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer');
 
 const {home_res} = require('../controller/file_object')
 
-router.route("/").get(home_res)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'temp_uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+
+router.route("/upload").post(upload.single('file'),(req,res)=>{
+    console.log(req.file)
+    const {bucket_name,desc} = req.body
+    console.log({bucket_name,desc})
+    res.send("Hello")
+})
 
 module.exports = router;
