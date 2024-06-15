a14a5401fef7ece48f6c549741bc31209b72f6e0
abhinav-217 <abhinavsaxena217@gmail.com>
2024-06-15 23:08:45 +0530

    middleware fixes

--- main.js
+++ main.js
@@ -4,34 +4,30 @@ const port = 3000
 const {connectDB} = require("./model/DbConnect")
 const dotenv = require('dotenv')
 const {verifyToken} = require("./Helpers/Helper")
-const { Readable } = require('stream');
-const fs = require('fs');
-const path = require('path');
 dotenv.config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    // try {
-    //     console.log(`${req.method} ${req.url}`);
-    //     if(req.url != '/user/login' && req.url != '/user/register' && req.url != '/mainBucket'){
-    //         console.log("Middle Ware Called");
-    //         auth_token = req.headers.auth_key
-    //         auth_details = verifyToken(auth_token)
-    //         isValidToken = auth_details.is_valid_client ?? false
-    //         if(isValidToken){
-    //             next()
-    //         }else{
-    //             res.send("Not valid token")
-    //         }
-    //     }else{
-    //         next()
-    //     }
-    // } catch (error) {
-    //     res.send("Not valid token")
-    // }
-    next()
+    try {
+        console.log(`${req.method} ${req.url}`);
+        if(req.url.includes('register') || req.url.includes('login') || req.url.includes('serve/file')){
+            next();
+        }else{
+            console.log("Middle Ware Called");
+            auth_token = req.headers.auth_key
+            auth_details = verifyToken(auth_token)
+            isValidToken = auth_details.is_valid_client ?? false
+            if(isValidToken){
+                next()
+            }else{
+                res.send("Not valid token")
+            }
+        }
+    } catch (error) {
+        res.send("Not valid token")
+    }
 });
 
 app.use("/", require('./router/home'))
